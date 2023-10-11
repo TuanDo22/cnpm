168a539f83a07689f8b3568f909437ca2a8310b1
TuanDo22 <doanhtuan220203@gmail.com>
2023-10-12 06:43:21 +0700

    Do An

--- src/main/java/com/example/projectse/entity/UserEntity.java
+++ src/main/java/com/example/projectse/entity/UserEntity.java
@@ -1,9 +1,6 @@
 package com.example.projectse.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class UserEntity implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String email;
     private String password;}
